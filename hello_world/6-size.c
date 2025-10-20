7641c832fdc1f53350b2060bedfb72ebf8564b02
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-20 14:42:26 +0000

    hw 6-1

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *prints exactly with proper grammar, but the outcome is a piece of art,
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int putchar(char, int, long int, long long int, float);
+printf("Size of a char: %c bytes(s)", char);
+printf("Size of a int: %d bytes(s)", int);
+printf("Size of a long int: %ld bytes(s)", long int);
+printf("Size of a char: %lld bytes(s)", long long int);
+return (0);
+}
