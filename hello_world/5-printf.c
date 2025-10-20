8ed92b9ce91eb0d05e2612e3aeac6baefe2f6714
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-20 14:17:11 +0000

    hw 5-1

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *prints exactly with proper grammar, but the outcome is a piece of art,
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
