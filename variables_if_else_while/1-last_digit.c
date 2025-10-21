c0ded2a576dc4bed46d0449a944070efefa922b6
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-21 08:53:33 +0000

    1-0

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*sortir le dernier chiffre
+*Return: always 0 (Succes)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	return (0);
+}
