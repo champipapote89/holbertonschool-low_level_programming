3845d87eadb14593a2f4991f5b66bd902d73d84c
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-21 13:55:09 +0000

    var 4-1

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*prints the alphabet in lowercase
+*Return: always 0 (Succes)
+*/
+int main(void)
+{
+char i = 'a';
+while (i <= 'z')
+{
+if(i != 'q' && i != 'e')
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
