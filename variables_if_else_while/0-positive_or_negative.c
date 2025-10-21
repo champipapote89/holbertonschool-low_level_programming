d2e1f72df7f41b346f9502e7cc75fc0bbaa80d7b
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-21 08:45:54 +0000

    var 0-2

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+*main - Entry point
+*assigner un nombre aléatoire
+*Return: always 0 (Succes)
+*/
 int main(void)
 {
-	int n=0;
+int n = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", n);
-    }
-    else
-    {
-        printf("%d is negative\n", n);
-    }
-	return (0);
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
