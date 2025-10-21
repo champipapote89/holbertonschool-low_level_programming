0909ffb72edbc961b8e82d771140f35f53a44293
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-21 14:30:09 +0200

    var 2-1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*prints the alphabet in lowercase
+*Return: always 0 (Succes)
+*/
+int main(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+putchar(i);
+return (0);
+}
+
