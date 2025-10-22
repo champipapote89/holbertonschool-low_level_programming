05cdb882c76c8d84d9c2ff0da88bb4060a1d57c8
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-22 14:22:16 +0000

    func 6_1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_abs - Entry point
+*@i: character to check
+*
+*Return: unsigned int i
+*
+*/
+int _abs(int i)
+{
+return (unsigned int i);
+}
