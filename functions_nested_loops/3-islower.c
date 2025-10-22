693e2fc224a8068cb13704baf8f2e1391488ff3a
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-22 12:13:10 +0000

    func 3_2

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,14 +5,15 @@
 
 /**
 *_islower - Entry point
-*check for lowercase character
+*@c: character to check
+*
+* Return: 1 if c is lowercase, 0 otherwise
 *Return: always 0 (Succes)
 */
 int _islower(int c)
 {
-if (c<91 && c>64);
+if (c < 91 && c > 64)
 {
-_putchar(c);
 return (1);
 }
 return (0);
