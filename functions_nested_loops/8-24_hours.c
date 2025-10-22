9c565cbde7443790f859f28e8cad0ca3125f32db
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-22 16:23:44 +0000

    func 8_0

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_last_digit - Entry point
+*@i: character to check
+*
+*Return: last digit
+*
+*/
+void jack_bauer(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+{
+_putchar (i + '0');
+}
+}
