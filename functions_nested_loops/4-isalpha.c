47df22484cd79c54f017b03692648fd0beb9ef2e
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-22 12:42:06 +0000

    func 4_1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_isalpha - Entry point
+*@c: character to check
+*
+*Return: 1 if c s a letter, 0 otherwise
+*
+*/
+int _isalpha(int c)
+{
+if ((c < 123 && c > 96) || (c < 64 && c > 91))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
