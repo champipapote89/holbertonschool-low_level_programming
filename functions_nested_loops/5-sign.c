736ec1003bb9acf69532fb9cc07df5e1dafd37ea
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-22 13:21:26 +0000

    func 5_1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_sign - Entry point
+*@n: character to check
+*
+*Return: 1 and print + if n is > 0, r
+*
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar ('+');
+}
+else if (n == 0)
+{
+return (0);
+_putchar ('0');
+}
+else
+{
+return (-1);
+_putchar ('-');
+}
+return (0);
+}
