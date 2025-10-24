f15a85a9cf9f1f283d27a821fb34edec734027b4
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-24 12:03:41 +0000

    var 12_1

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*prints all possible different combinations of two digits
+*Return: always 0 (Succes)
+*/
+int main(void)
+{
+	int n1 = 0;
+	int n2 = 0;
+	int n3 = 0;
+	int n4 = 1;
+
+	while (n1 <= 9 && n2 <= 8 && n3 <= 9 && n4 <= 9)
+	{
+		if (n3 + n4 > n1 + n2 && n3 >= n1)
+		{
+			putchar(48 + n1);
+			putchar(48 + n2);
+			putchar(' ');
+			putchar(48 + n3);
+			putchar(48 + n4);
+			if (n1 != 9 || n2 != 8 || n3 != 9 || n4 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		n4++;
+		if (n4 > 9)
+		{
+			n4 = 0;
+			n3++;
+		}
+		if (n3 > 9)
+		{
+			n3 = 0;
+			n4 = 0;
+			n2++;
+		}
+		if (n2 > 8)
+		{
+			n2 = 0;
+			n3 = 0;
+			n4 = 0;
+			n1++;
+		}
+	}
+	putchar('\n');
+return (0);
+}
