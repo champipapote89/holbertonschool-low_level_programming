2ac18855be901b7cb3f907e48d8d404ab5ddac73
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-24 07:40:54 +0000

    func 9_1

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,6 +14,7 @@ void times_table(void)
 	int i;
 	int k;
 	int n = 0;
+
 	for (i = 0; i < 10; i++)
 		{
 		for (k = 0; k <= 9; k++)
@@ -21,10 +22,22 @@ void times_table(void)
 			n = i * k;
 			if (n <= 9)
 			{
-				_putchar(48 + n);
+				if (k == 0)
+				{
+					_putchar(48 + n);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(48 + n);
+				}
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(48 + (n / 10));
 				_putchar(48 + (n % 10));
 			}
