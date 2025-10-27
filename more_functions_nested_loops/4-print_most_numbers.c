393bca4baed765f2c0cc9a74f9b7a36ca247bb62
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-27 09:44:38 +0000

    more f 3_2

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,17 +4,20 @@
 #include "main.h"
 
 /**
-*print_numbers - Entry point
-*prints numbers from 0 to 9
+*print_most_numbers - Entry point
+*prints numbers from 0 to 9 exept 2 and 4
 *Return: always 0 (Succes)
 */
 void print_numbers(void)
 {
-int i = '0';
+int i = 0;
 while (i <= '9')
 {
-_putchar(i);
-i++;
+	if (i != 2 || i != 4)
+	{
+		_putchar(i);
+		i++;
+	}
 }
 _putchar('\n');
 return;
