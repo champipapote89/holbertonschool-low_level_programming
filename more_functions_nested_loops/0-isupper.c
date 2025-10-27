fd3faf023c6b31a1838b767555b9051b9c0a273f
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-27 08:30:16 +0000

    more f 0_1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_isalpha - Entry point
+*@c: character to check
+*
+*Return: 1 if c s a letter, 0 otherwise
+*
+*/
+int _isupper(int c)
+{
+if (c < 91 && c > 64)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
