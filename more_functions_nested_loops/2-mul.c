db483e8cb5779623adc362be830d0cb0ffe96916
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-27 08:55:29 +0000

    more f 2_1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*mul - Entry point
+*@a: character to mul
+*@b: character to mul
+*
+*Return: a * b
+*
+*/
+int mul(int a, int b)
+{
+a = 0;
+b = 0;
+
+return (a *b);
+}
