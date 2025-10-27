9649e06f997b5ed817a60234ce55719f68d0e97a
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-27 15:23:00 +0000

    more f 10_1

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_triangle - Entry point
+*@size: size
+*print a triangle, followed by a new line.
+*Return: always 0 (Succes)
+*/
+
+void print_triangle(int size)
+{
+	int i = 0, j = 0, k = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < size)
+		{
+			j = 0;
+			while (j < size - i - 1)
+			{
+				_putchar(' ');
+				j++;
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
