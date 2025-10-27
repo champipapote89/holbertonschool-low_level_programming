b36277c8c50adba863605f0a91d045723e04c675
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-27 09:49:31 +0000

    more f 4_1

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,16 +8,16 @@
 *prints numbers from 0 to 9 exept 2 and 4
 *Return: always 0 (Succes)
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-int i = 0;
+int i = '0';
 while (i <= '9')
 {
-	if (i != 2 || i != 4)
+	if (i != '2' && i != '4')
 	{
 		_putchar(i);
-		i++;
 	}
+	i++;
 }
 _putchar('\n');
 return;
