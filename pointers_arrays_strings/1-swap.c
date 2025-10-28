a82e6a8053ded0fdc1de4e17e9767230ced48cc1
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-28 14:09:12 +0000

    pas 1_1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*swap_int - Entry point
+*@a: parametre à pointer
+*@b: parametre à pointer
+*swaps the values of two integers.
+*Return: always 0 (Succes)
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
