93ffca836adfe19f364977dbd9eb77facb7fda59
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-29 18:21:59 +0000

    mpas 2_0

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - add the string from src to dest
+ *@src: src
+ *@dest: dest
+ *@n: number max of char
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	
+}
