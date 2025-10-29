8ed8a6ff17bb337a3a729f543c6bbf1642a8f753
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-29 12:21:17 +0000

    pas 10_1

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -28,16 +28,24 @@ int _atoi(char *s)
 	int i = 0;
 	int result = 0;
 	int digit = 0;
+	int neg = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
+		{
+			neg += 1;
+		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			result = result * 10 + digit;
 		}
 		i++;
+	}
+	if (neg % 2 == 0)
+	{
+		result = result * -1;
 	}
 
 	return result;
