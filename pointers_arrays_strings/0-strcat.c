ff7ac6d64315f158aff21b552bf490c21ad30b37
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-29 16:09:29 +0000

    mpas 0_1

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - add the string from src to dest
+ *@src: src
+ *@dest: dest
+ *Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c = 0;
+	int d = 0;
+
+	while (dest[d] != '\0')
+	{
+		d++;
+	}
+	while (src[c] != '\0')
+	{
+		dest[d] = src[c];
+		c++;
+		d++;
+		
+	}
+	dest[d] = src[c];
+	return (dest);
+}
