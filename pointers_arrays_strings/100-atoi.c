1ce828ad37cf9a3f040be96e1e3e36ee82d6b1e6
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-29 12:15:21 +0000

    pas 10_0

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*_strlen - Entry point
+*@s: parametre à compter
+*return the length of a string.
+*Return: always 0 (Succes)
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * _atoi - convert a string to an integer
+ *@s: parametre à convertir
+ *Return: 0
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int result = 0;
+	int digit = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			result = result * 10 + digit;
+		}
+		i++;
+	}
+
+	return result;
+}
