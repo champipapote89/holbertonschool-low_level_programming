76c9e397dc6a73782be7d5dc980adef5fc5dedd8
champipapote89 <septuor.plomberie1s@icloud.com>
2025-10-31 08:23:51 +0000

    mpas 3_3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 < s2)
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-		return (-15);
+		i++;
 	}
-	else if (s1 == s2)
-	{
-		return (0);
-	}
-	else
-	{
-		return (15);
-	}
+	return (s1[i] - s2[i]);
 }
